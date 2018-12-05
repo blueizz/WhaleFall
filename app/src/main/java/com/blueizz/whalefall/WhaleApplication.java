e57a220c5d8066dcaa52cfc22ad9c0fd8a53097d
liurui <liurui@tuya.com>
2018-12-05 21:05:23 +0800

    接入LeakCanary...

---
+++ app/src/main/java/com/blueizz/whalefall/WhaleApplication.java
@@ -0,0 +1,17 @@
+package com.blueizz.whalefall;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class WhaleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
