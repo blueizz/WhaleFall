ca41121b35ce0634110f0205b88c684e2a1509d3
liurui <liurui@tuya.com>
2018-12-09 11:41:30 +0800

    imagetracer...

--- app/src/main/java/com/blueizz/whalefall/activity/MainActivity.java
+++ app/src/main/java/com/blueizz/whalefall/activity/MainActivity.java
@@ -3,8 +3,6 @@ package com.blueizz.whalefall.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
